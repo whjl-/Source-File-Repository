bc18b8dfb448306107bb9b8b82e37ec0385cad1e
William Langford <langfordwill@sky.com>
2012-08-20 09:22:03 +0200

    Update ClientTest.java

--- ClientTest.java
+++ ClientTest.java
@@ -3,10 +3,10 @@ import javax.swing.JOptionPane;
 
 public class ClientTest {
    public static void main(String[] args) {
-      String ipaddress;
-      ipaddress = JOptionPane.showInputDialog ( "Enter IP:" ); 
+      String ipaddres;
+      ipaddres = JOptionPane.showInputDialog ( "Enter IP:" ); 
       Client objec;
-      objec = new Client(ipaddress);
+      objec = new Client(ipaddres);
       objec.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
       objec.startRunning();
    }
